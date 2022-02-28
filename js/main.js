75759f4f4104cf2d9df5c50ff49419d24b970bd7
Timeryan <timeryan2002@gmail.com>
2022-02-28 19:46:30 +0500

    added functional

--- js/main.js
+++ js/main.js
@@ -1,26 +1,42 @@
 const title = document.querySelector(".input-title"),
       btn = document.querySelector(".send-task"),
       unDo = document.querySelector(".undo-tasks"),
-      desc = document.querySelector(".input-desc")
+      desc = document.querySelector(".input-desc"),
+      completedTask = document.querySelector(".completed-tasks")
 
 btn.addEventListener("click", createTask)
 
+//Функция создания задачи
 function createTask() {
+    //Отрабатывает только если есть заглавие задачи
     if (title.value != "") {
-        unDo.innerHTML += `
-        <div class="task">
-            <h2 class="task__title">${title.value}</h2>
-            <p class="task__desc">${desc.value}</p>
-            <button class="task-btn">Выполнено</button>
-        </div>
-        `
+        let task = document.createElement('div')
+        task.classList.add("task")
+
+        let taskTitle = document.createElement('h2')
+        taskTitle.classList.add("task__title")
+        taskTitle.innerText = title.value
+        
+        let taskDesc = document.createElement('p')
+        taskDesc.classList.add("task__desc")
+        taskDesc.innerHTML = desc.value
+
+        let taskBtn = document.createElement('button')
+        taskBtn.classList.add("task-btn")
+        taskBtn.innerText = "Выполнено"
+
+        task.append(taskTitle)
+        task.append(taskDesc)
+        task.append(taskBtn)
+        
+        unDo.append(task)
     }
 }
-document.addEventListener("click", switchTask)
-
-function switchTask() {
+document.addEventListener("click", (e) => switchTask(e))
+//Функция переноса задачи во вкладку выполненных
+function switchTask(e) {
     if (e.target.classList.contains("task-btn")) {
-        console.log(this)
+        completedTask.append(e.target.parentElement)
+        e.target.remove()
     }
 }
-
